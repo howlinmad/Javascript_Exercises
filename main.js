1d79fbd0b5231a320e7e7e9e1f34c2c4d6206873
Don Petersen <don.jess.petersen@gmail.com>
2015-12-15 09:43:10 -0800

    added new soltution

--- main.js
+++ main.js
@@ -106,4 +106,44 @@ minOutput.innerHTML += min(0,-10);
 // → -10
 //"Eloquent" solution:
 //Nailed it! Same as mine!
+
+/* 5. We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to check whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
+ Zero is even.
+ One is odd.
+ For any other number N, its evenness is the same as N - 2.
+Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a Boolean.
+*/
+function isEven(number) {
+   if (number < 0){
+     number = Math.abs(number);
+   }
+   if (number === 0)
+    return true;
+   else if (number === 1)
+    return false;
+   else {
+    number -= 2;    
+  	return isEven(number);
+    }
+}
+var recursive = document.getElementById("recursive1");
+recursive1.innerHTML += isEven(50);
+recursive = document.getElementById("recursive2");
+recursive2.innerHTML += isEven(75);
+recursive = document.getElementById("recursive3");
+recursive3.innerHTML += isEven(-1);
+//"Eloquent" solution:
+/*
+function isEven(n) {
+  if (n == 0)
+    return true;
+  else if (n == 1)
+    return false;
+  else if (n < 0)
+    return isEven(-n);
+  else
+    return isEven(n - 2);
+}
+*/
+
     
